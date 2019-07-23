60c03025ba140ccee4140bd9307463a7b7d8412b
xtaci <daniel820313@gmail.com>
2019-07-23 10:57:46 +0800

    fix os-compat

--- handle_stub.go
+++ handle_stub.go
@@ -7,14 +7,10 @@ import (
 	"net"
 )
 
-type TCPConn struct{ *net.UDPConn }
-
-// Dial connects to the remote TCP port,
-// and returns a single packet-oriented connection
-func Dial(network, address string) (*TCPConn, error) {
+func DialIP(network string, laddr, raddr *net.IPAddr) (rawHandle, error) {
 	return nil, errors.New("os not supported")
 }
 
-func Listen(network, address string) (*TCPConn, error) {
+func ListenIP(network string, laddr *net.IPAddr) (rawHandle, error) {
 	return nil, errors.New("os not supported")
 }
