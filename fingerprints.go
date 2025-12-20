cbf96359f63c20cfd3b27a9b3927d1ebc6f4182d
xtaci <imap@live.com>
2025-12-20 15:18:41 +0800

    update fingerprints.go to include new fingerprinting algorithm

--- fingerprints.go
+++ fingerprints.go
@@ -2,6 +2,7 @@ package tcpraw
 
 import (
 	"encoding/binary"
+	"math/rand"
 	"time"
 
 	"github.com/google/gopacket/layers"
@@ -40,14 +41,19 @@ var fingerPrintLinux = fingerPrint{
 	Options: []layers.TCPOption{
 		{1, 0, nil},
 		{1, 0, nil},
-		{8, 10, make([]byte, 10)}, // len = 10
+		{8, 10, make([]byte, 8)}, // len = 10
 	},
 	TTL: 64,
 }
 
 var defaultFingerPrint = fingerPrintLinux
 
+var bootTime time.Time
+
 func init() {
+	rand.Seed(time.Now().UnixNano())
+	// simulate a random boot time
+	bootTime = time.Now().Add(time.Duration(-rand.Intn(30*24)) * time.Hour)
 }
 
 func makeOption(optType FingerPrintType, options []layers.TCPOption, tsecr uint32) {
@@ -55,9 +61,10 @@ func makeOption(optType FingerPrintType, options []layers.TCPOption, tsecr uint3
 	case TypeLinux:
 		// Timestamps: Kind 8, Length 10
 		for i := range options {
-			if options[i].OptionType == 8 && len(options[i].OptionData) == 10 {
+			if options[i].OptionType == 8 && len(options[i].OptionData) == 8 {
 				// Use milliseconds for timestamp to be more realistic (RFC 7323)
-				nowMillis := time.Now().UnixNano() / 1e6
+				// And use uptime instead of unix timestamp
+				nowMillis := time.Since(bootTime).Milliseconds()
 				binary.BigEndian.PutUint32(options[i].OptionData[:4], uint32(nowMillis))
 				binary.BigEndian.PutUint32(options[i].OptionData[4:], tsecr)
 				break
